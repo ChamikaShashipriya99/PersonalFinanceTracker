396cb6d5ac00adde13267cf91e05e24b27db8dc5
USER™ <chamikashashipriya3@mail.com>
2025-04-20 18:23:09 +0530

    Initial commit

--- app/src/main/java/com/example/finance/ui/SplashActivity.kt
+++ app/src/main/java/com/example/finance/ui/SplashActivity.kt
@@ -4,18 +4,40 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import com.example.finance.R
+
+//class SplashActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_splash)
+//
+//        // Navigate to MainActivity after 2 seconds
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            startActivity(Intent(this, MainActivity::class.java))
+//            finish()
+//        }, 3000)
+//    }
+//}
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        // Navigate to MainActivity after 2 seconds
-        Handler(Looper.getMainLooper()).postDelayed({
+        val videoView = findViewById<VideoView>(R.id.videoView)
+        val videoPath = "android.resource://" + packageName + "/" + R.raw.investaria
+        val uri = videoPath.toUri()
+
+        videoView.setVideoURI(uri)
+        videoView.setOnCompletionListener {
+            // After the video finishes, move to the next screen
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-        }, 3000)
+        }
+
+        videoView.start()
     }
 }
